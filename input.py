9f3a390e680740afe8a4f04f2f6604607eb96e14
Deaconjoe <clouddjoe@gmail.com>
2019-10-22 11:09:22 +0100

    samples 1

--- input.py
+++ input.py
@@ -1,3 +1,12 @@
-user_input = input("Enter your name : ")
-message = "Hello %s!" % user_input 
-print (message)
+#user_input = input("Enter your name : ")
+# message = "Hello %s!" % user_input for python2 and python3 belo only python3
+#this called place holder 
+#message = f"Hello {user_input}"
+#print (message)
+
+#an other example !!
+name = input("Enter your name : ")
+surname = input("Enter your surname : ")
+
+message ="Hello %s %s" % (name ,surname )
+print(message)
