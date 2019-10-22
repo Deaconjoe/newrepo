2f8672e99fb610cb788ca1d93fcbd29df5294526
Deaconjoe <clouddjoe@gmail.com>
2019-10-22 11:24:25 +0100

    samples 1

--- input.py
+++ input.py
@@ -7,6 +7,8 @@
 #an other example !!
 name = input("Enter your name : ")
 surname = input("Enter your surname : ")
+when = "today"
 
-message ="Hello %s %s" % (name ,surname )
+#message ="Hello %s %s" % (name ,surname )
+message = f"Hello {name} {surname}. What's up {when}"
 print(message)
