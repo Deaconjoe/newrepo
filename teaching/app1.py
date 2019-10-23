8466c50b5de90d60edb786fa85ed82afd52ba187
Deaconjoe <clouddjoe@gmail.com>
2019-10-23 23:22:37 +0100

    progress

--- teaching/app1.py
+++ teaching/app1.py
@@ -1,12 +1,14 @@
 import json
 from difflib import get_close_matches
-
 data = json.load(open("data.json"))
-
 def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+    elif w.title() in data:
+       return data[w.title()]
+    elif w.upper() in data:
+        return data[w.upper()]
     elif len(get_close_matches(w, data.keys())) > 0:
         yn = input("Did you mean %s instead? Enter Y if yes, or N if no. " % get_close_matches(w, data.keys())[0])
         if yn == "Y":
@@ -20,4 +22,10 @@ def translate(w):
 
 word = input("Enter word: ")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output :
+        print(item)
+else :
+    print(output)
